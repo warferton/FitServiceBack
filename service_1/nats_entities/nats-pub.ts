fac92d20b5aaf1ac677f1007bc3b8dfe8754211a
warferton <warferton45@gmail.com>
2021-03-21 20:18:49 +0500

    Configured messaging between service_1 and service_2

---
+++ service_1/nats_entities/nats-pub.ts
@@ -0,0 +1,48 @@
+import { connect, NatsConnection, StringCodec} from 'nats';
+import { wait } from '../utils/time'
+
+//connect to NATS
+const server = 
+  //local + remote cluster
+{ 
+    servers: ["localhost:4222", "demo.nats.io:4443"],
+    name: 'union',
+    noEcho: true,
+    noRandomize: true,
+    timeout: 10 * 1000,
+    maxReconnectAttempts: 5
+};
+
+//init codec
+const sc = StringCodec();
+
+export const startPublisher =
+
+    (async () => {
+        let nc : NatsConnection;
+        try {
+            nc = await connect(server);
+        } catch (err) {
+            console.log(`error connecting to ${JSON.stringify(server)} \n ${err}`);
+            return;
+        };
+
+        console.log(`connected to ${nc.getServer()}`);
+
+        await wait(5 * 1000);
+
+        console.log("sending..");
+        
+        for(let i = 0; i < 15; i++){
+            nc.publish("messages.service.2", sc.encode("message__to_node_2 t:" + i));
+            console.log("sent...");
+            await wait(15 * 1000);
+        }
+
+        nc.publish("messages.service.2", sc.encode("close"));
+
+        await nc.flush();
+        await nc.drain();
+        nc.close();
+        
+    })();
---
+++ service_2/nats_entities/nats-sub.ts
@@ -0,0 +1,59 @@
+import { connect, NatsConnection, StringCodec } from 'nats';
+
+//connect to NATS
+const server = 
+  //local + remote cluster
+{ 
+    servers: ["localhost:4222", "demo.nats.io:4443"],
+    name: 'union',
+    noEcho: true,
+    timeout: 10 * 1000,
+    noRandomize: true,
+    maxReconnectAttempts: 5
+};
+
+//init codec
+const sc = StringCodec();
+  
+export const startSubscription =
+    (async () => {
+        let nc : NatsConnection;
+        try {
+            nc = await connect(server);
+        } catch (err) {
+            console.log(`error connecting to ${JSON.stringify(server)} \n ${err}`);
+            return;
+        };
+
+        console.log(`connected to ${nc.getServer()}`);
+
+        //Setting subscriptions
+        const sub = nc.subscribe("messages.service.2", {
+            timeout: 60 * 1000 // 60sec
+        });
+        try{
+            for await(const m of sub) {
+                let msg = sc.decode(m.data);
+
+                if(msg === 'close') break;
+                
+                console.log(`[${sub.getProcessed()}]: ${msg}  - pending: ${sub.getPending()}`);
+                // executeInsertMessage(msg);
+            }
+        }catch(err){
+            console.error(`Exception caught: ${err.message}`);
+        }
+        console.log("subscription closed");
+        
+        await nc.drain().catch(err => console.log(err));
+        nc.close();
+
+        const err = await nc.closed();
+        if(err){
+            console.log(`Error closing connection: ${err}`);    
+        }
+        return;
+        
+    })();
+ 
+// module.exports = startSubscription;
